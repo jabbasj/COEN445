4eccda52df36646bc011664731c193cfc6b6ad64
jabbasj <abbasj92@gmail.com>
2016-09-26 12:25:30 -0400

    basic udp client/server

---
+++ coen445client/coen445client/coen445client.cpp
@@ -0,0 +1,78 @@
+// coen445client.cpp : Defines the entry point for the console application.
+// Source: http://www.binarytides.com/udp-socket-programming-in-winsock/
+//
+
+#include "stdafx.h"
+
+#define SERVER "127.0.0.1"  //ip address of udp server
+#define BUFLEN 512  //Max length of buffer
+#define PORT 8888   //The port on which to listen for incoming data
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	struct sockaddr_in si_other;
+	int s, slen = sizeof(si_other);
+	char buf[BUFLEN];
+	char message[BUFLEN];
+	WSADATA wsa;
+
+	//Initialise winsock
+	printf("\nInitialising Winsock...");
+	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
+	{
+		printf("Failed. Error Code : %d", WSAGetLastError());
+		exit(EXIT_FAILURE);
+	}
+	printf("Initialised.\n");
+
+	//create socket
+	if ((s = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == SOCKET_ERROR)
+	{
+		printf("socket() failed with error code : %d", WSAGetLastError());
+		exit(EXIT_FAILURE);
+	}
+
+	//setup address structure
+	memset((char *)&si_other, 0, sizeof(si_other));
+	si_other.sin_family = AF_INET;
+	si_other.sin_port = htons(PORT);
+	si_other.sin_addr.S_un.S_addr = inet_addr(SERVER);
+
+	int count = 0;
+
+	//start communication
+	while (1)
+	{
+		//printf("Enter message : ");
+		//gets_s(message);
+		strncpy_s(message, std::to_string(count++).c_str(), sizeof(message)-1);
+		message[sizeof(message)-1] = 0;
+
+		//send the message
+		if (sendto(s, message, strlen(message), 0, (struct sockaddr *) &si_other, slen) == SOCKET_ERROR)
+		{
+			printf("sendto() failed with error code : %d", WSAGetLastError());
+			exit(EXIT_FAILURE);
+		}
+
+		//receive a reply and print it
+		//clear the buffer by filling null, it might have previously received data
+		memset(buf, '\0', BUFLEN);
+		//try to receive some data, this is a blocking call
+		if (recvfrom(s, buf, BUFLEN, 0, (struct sockaddr *) &si_other, &slen) == SOCKET_ERROR)
+		{
+			printf("recvfrom() failed with error code : %d", WSAGetLastError());
+			exit(EXIT_FAILURE);
+		}
+
+		puts(buf);
+
+		Sleep(500);
+	}
+
+	closesocket(s);
+	WSACleanup();
+
+	return 0;
+}
+
